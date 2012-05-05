2e24eb74df7e5c7ae5d418a374e6e9ebcbaa2780
Greg Bueno <Greg Bueno@192.168.1.100>
2012-05-05 12:55:37 -0700

    Adding Musicwhore.org Wordpress theme to source control. TODO: Mobile layout.

---
+++ vigilantmedia.com/wp/wp-content/themes/musicwhore/footer.php
@@ -0,0 +1,17 @@
+<nav id="page-nav" class="span-20 last">
+	<div class="span-4">&nbsp;</div>
+	<div id="page-nav-bar" class="span-13 prepend-1 append-1 last">
+<?php echo previous_posts_link(); ?>	
+<?php echo next_posts_link(); ?>
+	</div>
+</nav>
+
+			</div>
+			
+			<?php get_sidebar('footer');?>
+		</div>
+
+<?php wp_footer(); ?>
+			
+	</body>
+</html>
