6d1e401d0a9fdec0b1bd34a26a35ba5780156d1f
Greg Bueno <Greg Bueno@192.168.1.100>
2012-05-07 07:03:03 -0700

    Adding copyright footer (again.)

--- vigilantmedia.com/wp/wp-content/themes/musicwhore/footer.php
+++ vigilantmedia.com/wp/wp-content/themes/musicwhore/footer.php
@@ -1,10 +1,17 @@
-<nav id="page-nav" class="span-20 last">
-	<div class="span-4">&nbsp;</div>
-	<div id="page-nav-bar" class="span-13 prepend-1 append-1 last">
-<?php echo previous_posts_link(); ?>	
-<?php echo next_posts_link(); ?>
-	</div>
-</nav>
+				<nav id="page-nav" class="span-20 last">
+					<div class="span-4">&nbsp;</div>
+					<div id="page-nav-bar" class="span-13 prepend-1 append-1 last">
+				<?php echo previous_posts_link(); ?>	
+				<?php echo next_posts_link(); ?>
+					</div>
+				</nav>
+
+				<footer class="span-20 last">
+					<div class="span-4">&nbsp;</div>
+					<div class="span-13 prepend-1 append-1 last">
+					&#169; <?php echo gmdate('Y'); ?> Greg Bueno
+					</div>
+				</footer>
 
 			</div>
 			
