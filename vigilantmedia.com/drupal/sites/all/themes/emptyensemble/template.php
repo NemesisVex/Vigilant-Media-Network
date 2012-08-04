c5ddaacb4bb9b777bd2d920ab2afaf6009ebbee6
Greg Bueno <nemesisvex@hotmail.com>
2012-08-04 09:34:29 -0700

    Skeleton theme ready to launch.

---
+++ vigilantmedia.com/drupal/sites/all/themes/emptyensemble/template.php
@@ -0,0 +1,7 @@
+<?php
+
+function emptyensemble_preprocess_node(&$vars) {
+	$vars['submitted'] = '&#8212; Posted by <strong>' . $vars['node']->name . '</strong> on ' . date('F d, Y H:i', $vars['node']->created);
+}
+
+?>
