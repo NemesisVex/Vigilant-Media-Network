60ae6d95efccb369cfef431a311689cfdcea6733
Greg Bueno <nemesisvex@hotmail.com>
2012-08-10 20:03:06 -0700

    Added override for observantrecords.artist_info block.

--- vigilantmedia.com/drupal/sites/all/themes/emptyensemble/template.php
+++ vigilantmedia.com/drupal/sites/all/themes/emptyensemble/template.php
@@ -4,4 +4,28 @@ function emptyensemble_preprocess_node(&$vars) {
 	$vars['submitted'] = '&#8212; Posted by <strong>' . $vars['node']->name . '</strong> on ' . date('F d, Y H:i', $vars['node']->created);
 }
 
+function emptyensemble_theme() {
+	$theme = array(
+		'artist_info' => array(
+			'variables' => array(
+				'artists' => NULL,
+			),
+			'template' => 'emptyensemble.artist_info',
+		),
+		'album_info' => array(
+			'variables' => array(
+				'albums' => NULL,
+			),
+			'template' => 'emptyensemble.album_info',
+		),
+		'track_info' => array(
+			'variables' => array(
+				'tracks' => NULL,
+			),
+			'template' => 'emptyensemble.track_info',
+		)
+	);
+	return $theme;
+}
+
 ?>
