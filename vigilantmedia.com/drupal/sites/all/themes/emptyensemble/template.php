a6447cbc6f2bc178a2e4a4d96161542af9006f99
Greg Bueno <nemesisvex@hotmail.com>
2012-08-12 21:55:22 -0700

    Added template suggestion for taxonomy.

--- vigilantmedia.com/drupal/sites/all/themes/emptyensemble/template.php
+++ vigilantmedia.com/drupal/sites/all/themes/emptyensemble/template.php
@@ -4,28 +4,11 @@ function emptyensemble_preprocess_node(&$vars) {
 	$vars['submitted'] = '&#8212; Posted by <strong>' . $vars['node']->name . '</strong> on ' . date('F d, Y H:i', $vars['node']->created);
 }
 
-function emptyensemble_theme() {
-	$theme = array(
-		'artist_info' => array(
-			'variables' => array(
-				'artists' => NULL,
-			),
-			'template' => 'emptyensemble.artist_info',
-		),
-		'album_info' => array(
-			'variables' => array(
-				'albums' => NULL,
-			),
-			'template' => 'emptyensemble.album_info',
-		),
-		'track_info' => array(
-			'variables' => array(
-				'tracks' => NULL,
-			),
-			'template' => 'emptyensemble.track_info',
-		)
-	);
-	return $theme;
+function emptyensemble_preprocess_page(&$vars) {
+	if (arg(0) == 'taxonomy' && arg(1) == 'term' && is_numeric(arg(2))) {
+		$tid = arg(2);
+		$vars['theme_hook_suggestions'][] = 'page__taxonomy__' . $tid;
+	}
 }
 
 ?>
