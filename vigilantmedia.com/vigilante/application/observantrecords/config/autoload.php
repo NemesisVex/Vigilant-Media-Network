2fe805ecbca3663d7bcac341f2df8eff8cffa4c9
gbueno <gbueno@D-FW0W7J1.oris.washington.edu>
2012-08-15 14:48:53 -0700

    Upgrading to 2.1.2.

--- vigilantmedia.com/vigilante/application/observantrecords/config/autoload.php
+++ vigilantmedia.com/vigilante/application/observantrecords/config/autoload.php
@@ -37,7 +37,7 @@
 |
 */
 
-$autoload['packages'] = array(APPPATH.'third_party');
+$autoload['packages'] = array();
 
 
 /*
@@ -94,7 +94,7 @@ $autoload['config'] = array();
 |
 |	$autoload['language'] = array('lang1', 'lang2');
 |
-| NOTE: Do not include the "_lang" part of your file.  For example 
+| NOTE: Do not include the "_lang" part of your file.  For example
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
