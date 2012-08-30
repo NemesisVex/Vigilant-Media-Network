cd6e3b2a64bd16150aac666929f9e8526da2e7e1
gbueno <gbueno@D-FW0W7J1.oris.washington.edu>
2012-08-30 16:37:36 -0700

    Created mobile layout.

--- vigilantmedia.com/drupal/sites/all/themes/shinkyokuadvocacy/templates/page--node--1.tpl.php
+++ vigilantmedia.com/drupal/sites/all/themes/shinkyokuadvocacy/templates/page--node--1.tpl.php
@@ -1,10 +1,10 @@
-		<div id="container" class="container">
-			<div id="masthead" class="prepend-top centered">
+		<div id="container">
+			<div id="masthead" class="centered">
 				<header>
 					<?php if ($site_name): ?>
 					<h1 id="title">
 						<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-							<img src="<?php print $logo; ?>" alt="[<?php echo $site_name; ?>]" title="[<?php echo $site_name; ?>]" />
+							<img src="<?php print $logo; ?>" alt="[<?php echo $site_name; ?>]" title="[<?php echo $site_name; ?>]" id="shinkyoku-advocacy-logo" />
 						</a>
 					</h1>
 					<?php endif; ?>
@@ -12,7 +12,7 @@
 			</div>
 
 			<div id="content">
-				<div class="span-24 last">
+				<div class="full-column">
 				<?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
 				<a id="main-content"></a>
 				<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
