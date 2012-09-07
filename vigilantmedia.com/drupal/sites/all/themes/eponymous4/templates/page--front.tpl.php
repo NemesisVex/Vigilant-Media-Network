811facfe3f3633b40aac33400c3fc48ca061a8b9
Greg Bueno <nemesisvex@hotmail.com>
2012-09-06 18:07:58 -0700

    Tweaks to sidebar header, front page header, text decoration of blog headlines.

--- vigilantmedia.com/drupal/sites/all/themes/eponymous4/templates/page--front.tpl.php
+++ vigilantmedia.com/drupal/sites/all/themes/eponymous4/templates/page--front.tpl.php
@@ -83,7 +83,7 @@ if (!empty($album_aliases)):
 				<?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
 				<a id="main-content"></a>
 				<?php print render($title_prefix); ?>
-				<?php if ($title): ?><h2 class="title" id="page-title"><?php print $title; ?></h2><?php endif; ?>
+				<?php if ($title): ?><h2 class="title" id="page-title"><?php print $title; ?></h2><?php else: ?><h2 class="title" id="page-title">Blog</h2><?php endif; ?>
 				<?php print render($title_suffix); ?>
 				<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
 				<?php print render($page['help']); ?>
