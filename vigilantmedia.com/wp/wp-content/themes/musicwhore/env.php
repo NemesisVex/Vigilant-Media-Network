e42d0418a50205da3c97816408808dcb34930b2b
Greg Bueno <greg@gregbueno.com>
2012-10-24 14:57:44 -0700

    Removed env.php

--- vigilantmedia.com/wp/wp-content/themes/musicwhore/env.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('VIGILANTMEDIA_MT_DB_HOST', 'mysql.vigilantmedia.com');
-define('VIGILANTMEDIA_MT_DB_USER', 'vigilantmedia');
-define('VIGILANTMEDIA_MT_DB_PASSWORD', '3825crux');
-define('VIGILANTMEDIA_MT_DB_NAME', 'vigilantmedia');
