0288b46df97c25236ac3f9bd7a21f1df3d08f1f5
Greg Bueno <greg@gregbueno.com>
2013-04-26 15:26:56 -0700

    Adding model for audio_map table.

---
+++ observantrecords.com/www/application/models/obr_audio_map.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Obr_Audio
+ * 
+ * Obr_Audio is a model for Observant Records audio files.
+ *
+ * @author Greg Bueno
+ */
+
+class Obr_Audio_Map extends MY_Model {
+	
+	public $_table = 'ep4_audio_map';
+	public $primary_key = 'map_id';
+	public $has_many = array(
+		'tracks' => array(
+			'model' => 'Obr_Track',
+			'primary_key' => 'map_track_id',
+		),
+		'audio' => array(
+			'model' => 'Obr_Audio',
+			'primary_key' => 'map_audio_id',
+		),
+	);
+	
+	protected $soft_delete = true;
+	protected $soft_delete_key = 'map_deleted';
+	
+	public function __construct($params = null) {
+		parent::__construct($params);
+	}
+	
+}
