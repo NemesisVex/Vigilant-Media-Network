50675d9fe002378cfd55686b3d9425623b415122
Greg Bueno <greg@gregbueno.com>
2013-05-01 06:19:00 -0700

    Adding S3 PHP SDK.

---
+++ observantrecords.com/www/application/libraries/MyAwsS3.php
@@ -0,0 +1,38 @@
+<?php
+
+require(APPPATH . 'third_party/vendor/autoload.php');
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception;
+use Aws\S3\Iterator;
+
+/**
+ * MyAwsS3
+ *
+ * @author Greg Bueno
+ */
+class MyAwsS3
+{
+	protected $s3;
+	protected $s3_iterator;
+
+	public function __construct()
+	{
+		$this->s3 = Aws\S3\S3Client::factory(array(
+			'key' => ACCESS_KEY_ID,
+			'secret' => SECRET_ACCESS_KEY,
+		));
+	}
+
+	public function bucket_exists($bucket_name = null)
+	{
+		return $this->s3->doesBucketExist($bucket_name);
+	}
+
+	public function list_objects($args = array())
+	{
+		return $this->s3->getIterator('ListObjects', $args);
+	}
+}
+
+?>
